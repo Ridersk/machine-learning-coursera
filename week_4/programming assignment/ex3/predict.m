ec2fef02e9e2b1003c6ef6986173fff967561e87
Lucas <lucasmacielaguiar@gmail.com>
2020-02-16 13:14:10 -0300

    Finish Week4 Programming Assignment

--- week_4/programming assignment/ex3/predict.m
+++ week_4/programming assignment/ex3/predict.m
@@ -20,7 +20,25 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
+% Layer1 (Input Layer) = X values.
+% Note: examples was ordered from line to columns
+% transpose X from 5000x400 to 400x5000
+% out1: 401x5000 (layer_units X num_examples)
+out1 = transpose([ones(m, 1) X]);
 
+% Layer2 (Hidden Layer)
+% out2: 26x5000 (layer_units X num_examples)
+layer2_units = size(Theta1, 1);
+out2 = [ones(1, m); sigmoid(Theta1 * out1)];
+
+% Layer3 (Output Layer)
+% out3: 10x5000
+out3 = sigmoid(Theta2 * out2);
+
+[values, p] = max(out3, [], 1);
+
+% Transpose 1x5000 to 5000x1 (examples X labels)
+p = transpose(p);
 
 % =========================================================================
 
