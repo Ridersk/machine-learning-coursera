799172433ea8fa906f4f3000e3c23d26fb195485
Lucas <lucasmacielaguiar@gmail.com>
2020-09-29 12:39:40 -0300

    Erase trash on plotData.m in wee_1/programming_assignment

--- week_2/programming_assignment/ex1/plotData.m
+++ week_2/programming_assignment/ex1/plotData.m
@@ -16,11 +16,6 @@ figure; % open a new figure window
 %       appear as red crosses. Furthermore, you can make the
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
-
-data = load('ex1data1.txt');
-x = data(:, 1); y = data(:, 2);
-m = length(y);
-
 plot(x, y, 'rx', 'MarkerSize', 10);       % Plot the data
 xlabel('Population of city in 10,000s');  % Set the x-axis label
 ylabel('Profit in $10,000s');             % Set the y-axis label
